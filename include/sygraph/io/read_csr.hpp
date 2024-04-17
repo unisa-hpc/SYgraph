e21fc375ad7eed10d3105c0a1482e8cd535ec4b5
antonio-decaro <antonio.decaro99@outlook.it>
2024-04-17 17:58:31 +0200

    Refactor read_csr.hpp to add support for converting COO graph to CSR format

--- include/sygraph/io/read_csr.hpp
+++ include/sygraph/io/read_csr.hpp
@@ -74,6 +74,19 @@ sygraph::formats::CSR<value_t, index_t, offset_t> from_matrix(const std::string&
   return from_matrix<value_t, index_t, offset_t>(ifs);
 }
 
+/**
+ * @brief Converts a matrix in CSR format from a file to a CSR object.
+ * 
+ * This function reads a matrix in CSR format from a given file and converts it into a CSR object.
+ * The CSR object contains the row offsets, column indices, and non-zero values of the matrix.
+ * 
+ * @tparam value_t The value type of the matrix elements.
+ * @tparam index_t The index type used for column indices.
+ * @tparam offset_t The offset type used for row offsets.
+ * @param fname The name of the file containing the matrix in CSR format.
+ * @return The CSR object representing the matrix.
+ * @throws std::runtime_error if the file fails to open.
+ */
 template <typename value_t, typename index_t, typename offset_t>
 sygraph::formats::CSR<value_t, index_t, offset_t> from_csr(std::istream& iss) {
   size_t n_rows = 0;
@@ -111,13 +124,60 @@ sygraph::formats::CSR<value_t, index_t, offset_t> from_csr(std::istream& iss) {
   return sygraph::formats::CSR<value_t, index_t, offset_t>(row_offsets, column_indices, nnz_values);
 }
 
-template<typename value_t, typename index_t, typename offset_t>
+
+/**
+ * Reads a Compressed Sparse Row (CSR) graph from a file.
+ * 
+ * @tparam value_t The value type of the graph.
+ * @tparam index_t The index type of the graph.
+ * @tparam offset_t The offset type of the graph.
+ * @param fname The name of the file to read from.
+ * @return The CSR graph read from the file.
+ * @throws std::runtime_error if the file fails to open.
+ */
+template <typename value_t, typename index_t, typename offset_t>
 sygraph::formats::CSR<value_t, index_t, offset_t> from_csr(const std::string& fname) {
   std::ifstream ifs(fname);
   if (!ifs.is_open()) {
     throw std::runtime_error("Failed to open file: " + fname);
   }
   return from_csr<value_t, index_t, offset_t>(ifs);
+}
+
+/**
+ * Converts a COO (Coordinate List) representation of a graph to CSR (Compressed Sparse Row) format.
+ * 
+ * @tparam value_t The value type of the graph.
+ * @tparam index_t The index type of the graph.
+ * @tparam offset_t The offset type of the graph.
+ * 
+ * @param iss The input stream containing the COO representation of the graph.
+ * @return The CSR representation of the graph.
+ * 
+ * @throws std::runtime_error if the conversion is not implemented.
+ */
+template <typename value_t, typename index_t, typename offset_t>
+sygraph::formats::CSR<value_t, index_t, offset_t> from_coo(std::istream& iss) {
+  throw std::runtime_error("Not implemented");
+}
+
+/**
+ * Converts a COO file to a CSR format.
+ * 
+ * @tparam value_t The value type of the CSR matrix.
+ * @tparam index_t The index type of the CSR matrix.
+ * @tparam offset_t The offset type of the CSR matrix.
+ * @param fname The name of the COO file to read from.
+ * @return The CSR matrix converted from the COO file.
+ * @throws std::runtime_error if the file fails to open.
+ */
+template <typename value_t, typename index_t, typename offset_t>
+sygraph::formats::CSR<value_t, index_t, offset_t> from_coo(const std::string& fname) {
+  std::ifstream ifs(fname);
+  if (!ifs.is_open()) {
+    throw std::runtime_error("Failed to open file: " + fname);
+  }
+  return from_coo<value_t, index_t, offset_t>(ifs);
 }
 
 } // namespace csr
