6115c8340501725f69c35c2ce0202dc09c8f7900
antonio-decaro <antonio.decaro99@outlook.it>
2024-04-23 14:15:27 +0200

    Refactor CMakeLists.txt to build examples and add bfs example

---
+++ examples/bfs/bfs.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <sycl/sycl.hpp>
+#include <sygraph/sygraph.hpp>
+#include <chrono>
+
+int main(int argc, char** argv) {
+
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <path-to-graph>" << std::endl;
+    return 1;
+  }
+  std::string path = argv[1];
+
+  std::ifstream file(path);
+  if (!file.is_open()) {
+    std::cerr << "Error: could not open file " << path << std::endl;
+    return 1;
+  }
+
+  sycl::queue q {sycl::gpu_selector_v};
+
+  std::cout << "[*  ] Loading graph file" << std::endl;
+  auto coo = sygraph::io::coo::from_coo<uint, uint, uint>(file);
+  std::cout << "[** ] Converting to CSR" << std::endl;
+  auto csr = sygraph::io::csr::from_coo(coo);
+  std::cout << "[***] Building Graph" << std::endl;
+  auto G = sygraph::graph::build::from_csr<sygraph::memory::space::shared>(q, csr);
+  
+  using type_t = decltype(G)::vertex_t;
+  sycl::buffer<type_t> nighbours_count_buf(G.get_vertex_count());
+  sycl::buffer<type_t> first_neighbours_buf(G.get_vertex_count());
+
+  using load_balance_t = sygraph::operators::LoadBalancer;
+  using direction_t = sygraph::operators::Direction;
+  using frontier_view_t = sygraph::frontier::FrontierView;
+  using frontier_impl_t = sygraph::frontier::FrontierType;
+
+  auto inFrontier = sygraph::frontier::make_frontier<frontier_view_t::vertex, frontier_impl_t::bitmap>(q, G);
+  auto outFrontier = sygraph::frontier::make_frontier<frontier_view_t::vertex, frontier_impl_t::bitmap>(q, G);
+
+  bool* visited = sycl::malloc_shared<bool>(G.get_vertex_count(), q);
+  size_t* distances = sycl::malloc_shared<size_t>(G.get_vertex_count(), q);
+
+  auto device_graph = G.get_device_graph();
+
+  inFrontier.insert(0); // Start from vertex 0
+  distances[0] = 0; 
+  visited[0] = true;
+
+  auto start = std::chrono::high_resolution_clock::now();
+
+  std::cout << "[*] Running... " << std::endl;
+  while (!inFrontier.empty()) {
+    sygraph::operators::advance::push<load_balance_t::workitem_mapped>(G, inFrontier, outFrontier, [=](auto u, auto v) -> bool {
+      if (!(visited[v])) {
+        visited[v] = true;
+        distances[v] = distances[u] + 1;
+        return true;
+      }
+      return false;
+    });
+    inFrontier.swap_and_clear(outFrontier);
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::cout << "[*] Done" << std::endl;
+
+  for (size_t i = 0; i < G.get_vertex_count(); i++) {
+    std::cout << "Vertex " << i << " has distance " << distances[i] << std::endl;
+  }
+
+  std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
+
+  sycl::free(visited, q);
+  sycl::free(distances, q);
+}
--- include/sygraph/io/read_coo.hpp
+++ include/sygraph/io/read_coo.hpp
@@ -15,22 +15,26 @@ namespace io {
 namespace coo {
 
 /**
- * Converts a COO (Coordinate List) representation of a graph to CSR (Compressed Sparse Row) format.
+ * Retrieves the COO (Coordinate List) representation of a graph.
  * 
  * @tparam value_t The value type of the graph.
  * @tparam index_t The index type of the graph.
  * @tparam offset_t The offset type of the graph.
  * 
  * @param iss The input stream containing the COO representation of the graph.
- * @return The CSR representation of the graph.
- * 
- * @throws std::runtime_error if the conversion is not implemented.
+ * @return The COO representation of the graph.
  */
 template <typename value_t, typename index_t, typename offset_t = types::offset_t>
 sygraph::formats::COO<value_t, index_t, offset_t> from_coo(std::istream& iss) {
   // Initialize row pointers for CSR format
-  size_t num_edges;
-  iss >> num_edges;
+
+  // skip all comments that starts with %
+  while (iss.peek() == '%') {
+    iss.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+
+  size_t n_nodes1, n_nodes2, num_edges;
+  iss >> n_nodes1 >> n_nodes2 >> num_edges;
 
   std::vector<index_t> coo_row_indices(num_edges);
   std::vector<index_t> coo_col_indices(num_edges);
